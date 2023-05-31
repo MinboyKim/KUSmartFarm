ae4cfd4d16345669d7d18ed931ebb4816eeac2c2
MinboyKim <kdm3801@gmail.com>
2023-05-31 18:15:29 +0900

    del

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
           <Route path="/" element={<Main />}></Route>
           <Route path="/sensor/*" element={<Sensor />}></Route>
           <Route path="/scale/*" element={<Scale />}></Route>
-          {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
