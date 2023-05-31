1bb32e0cc8faea00493986b005cf4ea45ff3caf1
MinboyKim <kdm3801@gmail.com>
2023-05-31 21:18:40 +0900

    plus

--- src/pages/Sensor.js
+++ src/pages/Sensor.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Nav from "../components/Nav";
 
 const Sensor = () => {
   return (
     <div>
       <h1>Sensor</h1>
-      <Link to="/">Main</Link>
-      <br />
-      <Link to="/scale">Scale</Link>
+      <Nav />
     </div>
   );
 };
