f49adbff75d214cf6f42622b615078c62cae2666
MinboyKim <kdm3801@gmail.com>
2023-06-01 16:43:57 +0900

    Route and link fix

--- src/App.js
+++ src/App.js
@@ -4,27 +4,17 @@ import Main from "./pages/Main";
 import Sensor from "./pages/Sensor";
 import Scale from "./pages/Scale";
 import NotFound from "./pages/NotFound";
-import Header from "./pages/layout/Header";
-import Footer from "./pages/layout/Footer";
-import Contents from "./pages/layout/Contents";
-import Layout from "./pages/layout/Layout";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Layout>
-          <Header />
-          <Contents>
-            <Routes>
-              <Route path="/" element={<Main />}></Route>
-              <Route path="/sensor/*" element={<Sensor />}></Route>
-              <Route path="/scale/*" element={<Scale />}></Route>
-              <Route path="*" element={<NotFound />}></Route>
-            </Routes>
-          </Contents>
-          <Footer />
-        </Layout>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/sensor" element={<Sensor />}></Route>
+          <Route path="/scale" element={<Scale />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Contents from "./Contents";
+
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      <Contents>{children}</Contents>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
