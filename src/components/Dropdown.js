07be2267179cb4d1f74c1fab65f4f8fb3deae7b6
kangdaehoon <eogns47@konkuk.ac.kr>
2023-06-01 22:17:31 +0900

    nav bar complete

---
+++ src/components/Dropdown.js
@@ -0,0 +1,29 @@
+import React from "react";
+import style from "../css/Nav.module.css";
+
+const Dropdown = (props) => {
+  const [visibilityAnimation, setVisibilityAnimation] = React.useState(false);
+  const [repeat, setRepeat] = React.useState(null);
+
+  React.useEffect(() => {
+    if (props.visibility) {
+      setVisibilityAnimation(true);
+    } else {
+      setVisibilityAnimation(false);
+    }
+  }, [props.visibility]);
+
+  return (
+    <article
+      className={`components-dropdown ${
+        props.visibility
+          ? style["slide-fade-in-dropdown"]
+          : style["slide-fade-out-dropdown"]
+      }`}
+    >
+      {visibilityAnimation && props.children}
+    </article>
+  );
+};
+
+export default Dropdown;
--- src/pages/layout/Nav.js
+++ src/pages/layout/Nav.js
@@ -1,32 +1,56 @@
 import { Link } from "react-router-dom";
 import Navstyle from "../../css/Nav.module.css";
 import Profile from "../../components/Profile.js";
+import Dropdown from "../../components/Dropdown";
+import React from "react";
 
-const Nav = () => {
+const Nav = (props) => {
+  const [dropdownVisibility, setDropdownVisibility] = React.useState(false);
+  const [dropdownVisibility2, setDropdownVisibility2] = React.useState(false);
+
   return (
     <nav className={Navstyle.layout}>
       <Profile></Profile>
       <navbar className={Navstyle.navbar}>
         <ul className={Navstyle.navBarMenu}>
-          <li className={Navstyle.list}>
-            환경 센서
-            <ul className={Navstyle.link}>
-              <li>
-                <Link to="/sensor"></Link>
+          <li
+            className={Navstyle.list}
+            onClick={(e) => setDropdownVisibility(!dropdownVisibility)}
+          >
+            환경센서
+          </li>
+          <Dropdown visibility={dropdownVisibility}>
+            <ul className={`${Navstyle.link} ${Navstyle.customUl}`}>
+              <li className={Navstyle.list2}>
+                <Link to="/sensor">센서1</Link>
               </li>
               <li>
-                <Link to="/sensor"></Link>
+                <Link to="/sensor">센서2</Link>
               </li>
               <li>
-                <Link to="/sensor"></Link>
+                <Link to="/sensor">센서3</Link>
               </li>
             </ul>
+          </Dropdown>
+          <li
+            className={Navstyle.list}
+            onClick={(e) => setDropdownVisibility2(!dropdownVisibility2)}
+          >
+            육계 저울
           </li>
-          <li>
-            <Link to="/scale" className={Navstyle.list}>
-              육계 저울
-            </Link>
-          </li>
+          <Dropdown visibility={dropdownVisibility2}>
+            <ul className={`${Navstyle.link} ${Navstyle.customUl}`}>
+              <li>
+                <Link to="/scale">저울1</Link>
+              </li>
+              <li>
+                <Link to="/scale">저울2</Link>
+              </li>
+              <li>
+                <Link to="/scale">저울3</Link>
+              </li>
+            </ul>
+          </Dropdown>
         </ul>
       </navbar>
     </nav>
