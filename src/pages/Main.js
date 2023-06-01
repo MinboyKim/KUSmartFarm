979db2cd774a055d7ecca134115287e99644ff2d
MinboyKim <kdm3801@gmail.com>
2023-06-01 16:56:17 +0900

    Main Page - Log in

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,26 @@
 import React from "react";
 import classes from "../css/Main.module.css";
-import Nav from "./layout/Nav";
-import Card from "../components/Card";
-import Container from "../css/Container.module.css";
 import { Link } from "react-router-dom";
 const Main = () => {
+  const handleLogin = () => {
+    var id = document.getElementById("id");
+    var pwd = document.getElementById("pwd");
+    if (id.value === "kusmartfarm" && pwd.value === "kukuku") {
+      window.location.replace("/sensor");
+    } else {
+      alert("아이디 또는 비밀번호가 틀렸습니다.");
+      id.value = "";
+      pwd.value = "";
+    }
+  };
+
   return (
     <div>
       <Link to="/sensor">링크</Link>
+      <br />
+      <input id="id" type="text" placeholder="아이디" />
+      <input id="pwd" type="password" placeholder="비밀번호" />
+      <button onClick={handleLogin}>로그인</button>
     </div>
   );
 };
