32e1e6609310c6220d3fe95c118289eee356efe3
MinboyKim <kdm3801@gmail.com>
2023-06-02 17:40:38 +0900

    delete park

--- src/pages/layout/Header.js
+++ src/pages/layout/Header.js
@@ -1,26 +1,23 @@
-import classes from "../../css/Header.module.css";
+import classes from '../../css/Header.module.css';
 
 const Header = () => {
   return (
-    <header className={classes.wrapper}>
-      <div className={classes.left}>
-        <img src={require("../../images/char.png")} width="150px"></img>
+      <header className = {classes.wrapper}><div className = {classes.left}>
+      <img src = {require('../../images/char.png')} width = '150px'>
+      </img>
         <h1>건국 Smart Farm</h1>
       </div>
       <div>
         <nav className={classes.gnb}>
           <ul className={classes.ul}>
             <li>공지사항</li>
-            <li>사용자 관리</li>
+      <li>사용자 관리</li>
             <li>설정</li>
-            <li>알림</li>
-            <li>박대원</li>
-            <li>menu</li>
-          </ul>
+      <li>알림</li>
+            <li>menu</li></ul>
         </nav>
       </div>
-    </header>
-  );
+    </header>);
 };
 
 export default Header;
